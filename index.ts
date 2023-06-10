f6c4650cb136904c1157ff5491529a8547a33110
Rhys Johns <rhysjohnsdev@gmail.com>
2023-06-10 23:50:51 +1000

    Added a simple test to make sure that the openai library works

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("It's me, Ducky!");
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "davinci",
+  prompt:
+    "This is a test request, could you tell me what kind of model you are?",
+  temperature: 0,
+});
+
+console.log(response.data);
