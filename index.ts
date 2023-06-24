51973b5eeb8687874467d10a0dd61fbc21013ca5
Rhys Johns <rhysjohnsdev@gmail.com>
2023-06-24 17:59:07 +1000

    Made ducky keep asking for what you want

--- index.ts
+++ index.ts
@@ -4,23 +4,29 @@ import { createDuckySystemPrompt, createOpenAiClient } from "./openAiClient.ts";
 
 // Example prompts for testing:
 //  1. "How would I view my current branch and it's previous commits in a nice tree format?"
-const prompt = await input({
-  message: "Hi I'm Ducky! What are you trying to build?",
-});
+while (true) {
+  const prompt = await input({
+    message: "Hi I'm Ducky! What are you trying to build?",
+  });
 
-const openAiClient = createOpenAiClient();
-const duckySystemPrompt = createDuckySystemPrompt();
+  if (prompt === "exit") {
+    break;
+  }
 
-const response = await openAiClient.createChatCompletion({
-  model: "gpt-3.5-turbo",
-  messages: [
-    ...duckySystemPrompt,
-    {
-      role: "user",
-      content: prompt,
-    },
-  ],
-});
+  const openAiClient = createOpenAiClient();
+  const duckySystemPrompt = createDuckySystemPrompt();
 
-console.log(response.data);
-console.log(response.data.choices[0].message);
+  const response = await openAiClient.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      ...duckySystemPrompt,
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  console.log(response.data);
+  console.log(response.data.choices[0].message);
+}
