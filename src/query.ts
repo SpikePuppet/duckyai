7616efe5fd5bf6afc38efbc5d4723c1679b8d82b
Rhys Johns <rhysjohnsdev@gmail.com>
2023-06-25 10:18:16 +1000

    First pass of a query ability

--- src/query.ts
+++ src/query.ts
@@ -1,3 +1,34 @@
-export async function query() {
-  console.log("Ducky is querying!");
+import chalk from "chalk";
+import {
+  OpenAIChatCompletionMessage,
+  createDuckySystemPrompt,
+  createOpenAiClient,
+} from "./openAiClient.ts";
+
+export async function query(prompt: string) {
+  const userPrompts: OpenAIChatCompletionMessage[] = [];
+
+  const openAiClient = createOpenAiClient();
+  const duckySystemPrompt = createDuckySystemPrompt();
+
+  userPrompts.push({
+    role: "user",
+    content: prompt,
+  });
+
+  const response = await openAiClient.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [...duckySystemPrompt, ...userPrompts],
+  });
+
+  const command = response.data.choices[0].message?.content;
+  if (command === undefined) {
+    console.log(
+      chalk.red(
+        "I'm not quite sure how to help with that. Can you try rephrasing?",
+      ),
+    );
+  }
+
+  return command;
 }
