6a100de3c0c55e3bbd81bd3acaa5ac96507cf73b
Rhys Johns <rhysjohnsdev@gmail.com>
2023-06-30 22:48:12 +1000

    First pass at an implementation of an interactive chat

--- src/interactivePrompt.ts
+++ src/interactivePrompt.ts
@@ -1,27 +1,36 @@
 import chalk from "chalk";
-import {
-  OpenAIChatCompletionMessage,
-  createOpenAiClient,
-  createDuckySystemPrompt,
-} from "./openAiClient.js";
 import enquirer from "enquirer";
+import { ConversationChain } from "langchain/chains";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder,
+} from "langchain/prompts";
+import { SystemMessagePromptTemplate } from "langchain/prompts";
+import { BufferMemory } from "langchain/memory";
 
-export interface ChatPrompt {
+export interface UserChatPrompt {
   userQuestion: string;
 }
 
 export async function interactivePrompt() {
+  const openAIApiKey = process.env.OPENAI_API_KEY;
+  const model = new ChatOpenAI({
+    openAIApiKey,
+    modelName: "gpt-3.5-turbo-0613",
+    temperature: 0.1,
+  });
   const { prompt } = enquirer;
+
   console.log(chalk.green("Hi! I'm Ducky!"));
   console.log(
     chalk.white("I'm here to help you with your command line needs!"),
   );
   console.log(chalk.white("How can I help?"));
 
-  const userPrompts: OpenAIChatCompletionMessage[] = [];
-
   while (true) {
-    const input: ChatPrompt = await prompt({
+    const input: UserChatPrompt = await prompt({
       type: "input",
       name: "userQuestion",
       message: "How can I help?",
@@ -35,29 +44,21 @@ export async function interactivePrompt() {
       continue;
     }
 
-    const openAiClient = createOpenAiClient();
-    const duckySystemPrompt = createDuckySystemPrompt();
+    const commandPrompt = ChatPromptTemplate.fromPromptMessages([
+      SystemMessagePromptTemplate.fromTemplate(
+        "You are DuckyAI, a helpful AI assistant that writes shell commands for users. You are given a description of what the user wants to do, and you should output a valid shell command in response. Do not output any of the context around the command. If you do have some context, omit it entirely.",
+      ),
+      new MessagesPlaceholder("history"),
+      HumanMessagePromptTemplate.fromTemplate("{userInput}"),
+    ]);
 
-    userPrompts.push({
-      role: "user",
-      content: input.userQuestion,
+    const chain = new ConversationChain({
+      memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+      prompt: commandPrompt,
+      llm: model,
     });
+    const response = await chain.call({ userInput: input.userQuestion });
 
-    const response = await openAiClient.createChatCompletion({
-      model: "gpt-3.5-turbo-0613",
-      messages: [...duckySystemPrompt, ...userPrompts],
-    });
-
-    const command = response.data.choices[0].message?.content;
-    if (command === undefined) {
-      console.log(
-        chalk.red(
-          "I'm not quite sure how to help with that. Can you try rephrasing?",
-        ),
-      );
-      continue;
-    }
-
-    console.log(command);
+    console.log(JSON.stringify(response.response));
   }
 }
