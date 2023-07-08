b45e7e517e5d8fa8a7e1eaa5cc49f39e149b2667
Rhys Johns <rhysjohnsdev@gmail.com>
2023-07-08 15:56:04 +1000

    Removed code splitting for now

--- tsup.config.js
+++ tsup.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["index.ts"],
   format: ["cjs", "esm"],
-  splitting: true,
+  splitting: false,
   sourcemap: true,
   clean: true,
   bundle: true,
